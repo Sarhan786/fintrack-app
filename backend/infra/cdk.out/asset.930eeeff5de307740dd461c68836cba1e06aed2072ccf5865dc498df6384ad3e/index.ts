c44a176415143a63c11675b3d176e17e275101e6
Sarhan786 <sarahan786@gmail.com>
2025-08-11 22:03:40 +0530

    finTrack: update remove cdk.out

--- backend/infra/cdk.out/asset.930eeeff5de307740dd461c68836cba1e06aed2072ccf5865dc498df6384ad3e/index.ts
+++
@@ -1,7 +0,0 @@
-export const handler = async (event: any) => {
-  console.log("Lambda invoked with event:", event);
-  return {
-    statusCode: 200,
-    body: JSON.stringify('Hello from the users-service!'),
-  };
-};
